a367e39f9967854411e4411e82624594b1e0f845
Nina Guo <ninaguo1305@gmail.com>
2022-11-18 20:52:55 +0800

    add students_nina_guo     video for capstone     screeshot for github

--- src/ions/data/students.js
+++ src/ions/data/students.js
@@ -16,18 +16,18 @@ const students = [
 	},
 	{
 		// The ID is used to find images and videos: firstname_lastname
-		id: "max_muster",
+		id: "nina_guo",
 		// Name displayed on slide
-		name: "Max Muster",
-		app: "Capstone 9000",
+		name: "Nina Guo",
+		app: "capstone-NFT-2022",
 		// Optional. If set, the Domain will be shown on the slide, e.g. https://my-capstone.herokuapp.com
-		domain: "https://my-capstone.herokuapp.com",
+		domain: "https://capstone-nft-2022.herokuapp.com/",
 		// Will be printed as written here
-		techStack: ["Java", "Spring", "Maven", "TypeScript", "React", "Git", "MongoDB", "Rest API", "JUnit", "Docker", "Mockito"],
-		commits: 12,
-		pullRequests: 2,
+		techStack: ["Java", "Spring", "Maven", "TypeScript", "React", "Git", "MongoDB", "Rest API", "JUnit", "Docker", "Mockito","CSS","Postman"],
+		commits: 53,
+		pullRequests: 16,
 		//'mobile', 'tablet_portrait', 'tablet_landscape', 'desktop'
-		device: "mobile/desktop",
+		device: "desktop",
 	},
 ];
 
