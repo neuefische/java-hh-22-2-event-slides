1914d4317c9220dccd6d775011f7dcf8e7780ad9
hunjoonrhee <hunjoonrhee90@gmail.com>
2022-11-18 16:42:56 +0100

    Upload hunjoon_rhee

--- src/ions/data/students.js
+++ src/ions/data/students.js
@@ -16,16 +16,16 @@ const students = [
 	},
 	{
 		// The ID is used to find images and videos: firstname_lastname
-		id: "max_muster",
+		id: "hunjoon_rhee",
 		// Name displayed on slide
-		name: "Max Muster",
-		app: "Capstone 9000",
+		name: "Hunjoon Rhee",
+		app: "Running Route Advisor",
 		// Optional. If set, the Domain will be shown on the slide, e.g. https://my-capstone.herokuapp.com
-		domain: "https://my-capstone.herokuapp.com",
+		domain: "https://rra-capstone.herokuapp.com/",
 		// Will be printed as written here
-		techStack: ["Java", "Spring", "Maven", "TypeScript", "React", "Git", "MongoDB", "Rest API", "JUnit", "Docker", "Mockito"],
-		commits: 12,
-		pullRequests: 2,
+		techStack: ["Java", "Spring", "Maven", "TypeScript", "React", "Git", "MongoDB", "Rest API", "JUnit", "Docker", "Mockito", "CSS", "CI", "Sonar"],
+		commits: 91,
+		pullRequests: 22,
 		//'mobile', 'tablet_portrait', 'tablet_landscape', 'desktop'
 		device: "mobile/desktop",
 	},
